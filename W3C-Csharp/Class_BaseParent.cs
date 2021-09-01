1f8e820e7aad7746b6db9fac85e0afe80314ffb0
Ryan Cunningham <ryan.cunningham@acadiahealthcare.com>
2021-09-01 08:29:29 -0500

    Add project files.

---
+++ W3C-Csharp/Class_BaseParent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace c_sharp_playground
+{
+    public abstract class Class_BaseParent
+    {
+        public string Make { get; set; } = "test";
+        public abstract void honk();
+    }
+}
